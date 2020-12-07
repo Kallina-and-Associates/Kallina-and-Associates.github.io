8dffc4dea39ebe528b7d39f6043cd476bd942d44
Deb Kallina <dkallina@gmail.com>
2020-12-07 17:36:16 -0500

    Add notes

---
+++ .cmd
@@ -0,0 +1,11 @@
+This project uses Github Pages. To view the settings, go to the
+Kallina-and-Associates.github.io repo in Github, and click on Settings.
+
+We are using Formspree to send the "Contact Us" form. The username is
+admin@charitableplanning.com but any submissions will go to administrator@kallinalaw.com.
+
+To test locally,
+>  bundle exec jekyll serve
+and visit localhost:4000 in the browser
+
+To publish changes, simply merge your latest changes in to master / main.
